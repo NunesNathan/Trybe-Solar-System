06f8528a45801123e47ea948e7bc5568e6964f05
NunesNathan <nunesxd7@hotmail.com>
2021-10-29 17:52:47 -0300

    C7 - Requisitos 9 e 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions
+          .map((each) => (<MissionCard
+            key={ each.name }
+            name={ each.name }
+            year={ each.year }
+            country={ each.country }
+            destination={ each.destination }
+          />))}
       </div>
     );
   }
